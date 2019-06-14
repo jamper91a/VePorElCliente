9db1b1c0c303caf5eb3fb45cb6cc4727fe7c1fb4
jamper91a <jamper91a@gmail.com>
2019-06-14 15:02:39 -0500

    Se cambian textos

--- src/providers/util.ts
+++ src/providers/util.ts
@@ -91,7 +91,7 @@ export class Util{
 
 
     this.google_api_key = "AIzaSyDvZFVr2cdCCVyLmMBg0-8MaJTJDaHD8pE";
-    this.version = "2.9.3";
+    this.version = "2.9.4";
     this.puntos = "0";
   }
 
